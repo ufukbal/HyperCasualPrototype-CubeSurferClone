7e4ea2fb8b34d4b1c89c45646c0875a689ecdb30
ufuk bal <balufuk91@gmail.com>
2021-04-29 00:29:55 +0300

    prototype challenge in 8 hours

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    public Vector3 Offset;
+
+    private void LateUpdate()
+    {
+        if (_target != null)
+            FollowTarget();
+    }
+    private void FollowTarget()
+    {
+        transform.position = new Vector3(_target.position.x + Offset.x, transform.position.y, _target.position.z + Offset.z);
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    public GameObject[] cubePrefabs;
+
+    public GameObject environment;
+
+    private List<GameObject> environmentPool = new List<GameObject>();
+    private List<GameObject> CubePool;
+
+
+    private float levelStartZ = -2.3f;
+    private float levelEndZ = 43f;
+    private float xBoundary = 0.155f;
+
+    [Range(0.05f, 3f)]
+    public float spawnZ = 0.1f;
+    private int prefabIndex;
+    private Vector3 cubeSpawnPoint;
+
+    private void Awake()
+    {
+        #region Singleton
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(Instance.gameObject);
+        }
+
+        #endregion //Singleton
+    }
+    private void Start()
+    {
+        CreateLevel(Vector3.zero);
+    }
+    public void CreateLevel(Vector3 envPos)
+    {
+        if (envPos.z / 45 > 1)
+        {
+            SendEnvironmentToPool();
+        }
+
+        if (GetEnvironmentFromPool())
+        {
+            InstantiatePoolItem(GetEnvironmentFromPool(), envPos, Quaternion.identity);
+
+        }
+        else
+        {
+            GameObject.Instantiate(environment, envPos, Quaternion.identity);
+        }
+        for (float z = levelStartZ + envPos.z; z < levelEndZ + envPos.z; z += spawnZ)
+        {
+            //TO-DO: Implement Object Pool for Cubes
+            prefabIndex = Random.Range(0, cubePrefabs.Length);
+            cubeSpawnPoint = new Vector3(Random.Range(-xBoundary, xBoundary), cubePrefabs[prefabIndex].transform.position.y, z);
+
+            GameObject.Instantiate(cubePrefabs[prefabIndex], cubeSpawnPoint, Quaternion.identity);
+
+        }
+
+    }
+
+    private void SendEnvironmentToPool()
+    {
+        GameObject unusedEnvironment = GameObject.FindGameObjectWithTag("Environment");
+        environmentPool.Add(unusedEnvironment);
+        unusedEnvironment.SetActive(false);
+
+    }
+
+    private GameObject GetEnvironmentFromPool()
+    {
+        GameObject poolItem;
+        if (environmentPool.Count == 0 || environmentPool == null) return null;
+        poolItem = environmentPool[0];
+        return poolItem;
+    }
+
+    private void InstantiatePoolItem(GameObject gameObj, Vector3 pos, Quaternion rot)
+    {
+        gameObj.transform.position = pos;
+        gameObj.transform.rotation = rot;
+        gameObj.SetActive(true);
+        environmentPool.Remove(gameObj);
+    }
+
+}
